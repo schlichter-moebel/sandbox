d71f1ae09f319e1938b9fe7ece23f9530b57eb7a
Vitalisch <vitali.scharov@gmail.com>
2015-03-04 14:46:41 +0100

    v.0.2-PreAlfa

--- new.php
+++ new.php
@@ -10,7 +10,7 @@ if ($_POST) {
     if (!mysql_connect($config['hostname'], $config['username'], $config['password'])) die('Verbindung schlug fehl: ' . mysql_error());
     mysql_select_db($config['database']);
 
-    $sql = "INSERT INTO posts (id, title, slug, text, created, status) VALUES ('', $titel, $slug, $text, '', '')";
+    $sql = "INSERT INTO posts (id, title, slug, text, created, status) VALUES ('', '$titel', '$slug', '$text', '', '')";
 
     if ($result = mysql_query($sql)) {
         echo "New record created successfully. <a href='index.php'>Startseite</a>" ;
@@ -32,8 +32,22 @@ else
         <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,700' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'
     </head>
+    <style> ul li {list-style-type: none; } </style>
     <body>
+    <article>
     <div class="row">
+        <div class="one-quarter meta">
+            <div class="thumbnail">
+                <img src="dropplets.jpg" />
+            </div>
+            <ul>
+                <li>New Post</li>
+                <li><a href="index.php">Back</a></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+        <div class="three-quarters post">
         <form action="new.php" method="post">
              <ul>
                 <li><label>Titel</label></li>
@@ -45,10 +59,11 @@ else
              </ul>
              <ul>
                 <li><label>Text</label></li>
-                <li><textarea name="text"></textarea></li>
+                <li><textarea name="text" rows="5" cols="40"></textarea></li>
              </ul>
              <input type="submit" value="Senden">
         </form>
+        </div>
     </div>
     </article>
     </body>
